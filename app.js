44582226e3f94e4d95999bf229668c53b63fb961
romyedith <romyedith@gmail.com>
2024-02-25 22:33:20 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-  // Carga datos de Amazon
   Papa.parse("amazon_data.csv", {
       download: true,
       header: true,
-      complete: function(amazonResults) {
-          let dataHTML = "<table>";
-          dataHTML += "<tr><th>Nombre del Producto</th><th>Precio</th></tr>";
-          amazonResults.data.forEach(function(row) {
-            dataHTML += `<tr><td>${row["producto"]}</td><td>${row["precio"]}</td></tr>`;
-          });
-          dataHTML += "</table>";
-          document.getElementById("productos-amazon").innerHTML = dataHTML;
+      complete: function(results) {
+          displayData(results.data, "productosAmazon");
       }
   });
 
-  // Carga datos de Mercado Libre
   Papa.parse("mercado_data.csv", {
       download: true,
       header: true,
-      complete: function(mercadoLibreResults) {
-          let dataHTML = "<table>";
-          dataHTML += "<tr><th>Nombre del Producto</th><th>Precio</th></tr>";
-
-          // **Solución al problema de "undefined"**
-          // Se verifica si la columna "nombre" existe en los datos de Mercado Libre
-          if (mercadoLibreResults.data[0].hasOwnProperty("producto")) {
-              mercadoLibreResults.data.forEach(function(row) {
-                  dataHTML += `<tr><td>${row["producto"]}</td><td>${row["valor"]}</td></tr>`;
-              });
-          } else {
-              // Si la columna "nombre" no existe, se muestra un mensaje de error
-              dataHTML += "<tr><td colspan='2'>Error: No se encontró la columna 'nombre' en el archivo CSV de Mercado Libre.</td></tr>";
-          }
-
-          dataHTML += "</table>";
-          document.getElementById("mercado-libre").innerHTML = dataHTML;
+      complete: function(results) {
+          displayData(results.data, "productosMercado");
       }
   });
+
+  function displayData(data, containerId) {
+      let dataHTML = "<table>";
+      dataHTML += "<tr><th>Nombre del Producto</th><th>Precio</th></tr>";
+          
+      data.forEach(function(row) {
+          dataHTML += `<tr><td>${row["producto"]}</td><td>${row["precio"]}</td></tr>`;
+      });
+      
+      dataHTML += "</table>";
+      document.getElementById(containerId).innerHTML = dataHTML;
+  }
 });
+
